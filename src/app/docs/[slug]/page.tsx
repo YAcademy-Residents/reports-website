df908e09fc69fd82f7b1c756b189901480211e44
nullity <70228821+nullity00@users.noreply.github.com>
2025-05-24 18:49:14 +0400

    .tech to .com

--- src/app/docs/[slug]/page.tsx
+++ src/app/docs/[slug]/page.tsx
@@ -1,22 +1,22 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function RedirectPage() {
   const pathname = usePathname();
   const [countdown, setCountdown] = useState(3);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const slug = pathname.replace('/docs/', '');
+    if (typeof window !== "undefined") {
+      const slug = pathname.replace("/docs/", "");
       if (!slug) return;
 
       const interval = setInterval(() => {
         setCountdown((prev) => {
           if (prev === 1) {
             clearInterval(interval);
-            window.location.href = `https://reports.electisec.tech/reports/${slug}`;
+            window.location.href = `https://reports.electisec.com/reports/${slug}`;
           }
           return prev - 1;
         });
@@ -27,27 +27,42 @@ export default function RedirectPage() {
   }, [pathname]);
 
   return (
-    <div style={{ 
-      display: 'flex', 
-      flexDirection: 'column', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      height: '100vh', 
-      backgroundColor: 'white', 
-      textAlign: 'center' 
-    }}>
-      <h1 style={{
-        color: "black",
-        fontSize: '2.5rem'
-      }}>yAcademy</h1>
-      <p style={{ fontSize: '1.5rem', margin: '10px 0', color: "grey" }}>is now</p>
-      <img src="/electisec-logo.png" alt="Electisec Logo" width={250} height={250} />
-      <p  style={{
-        color: "grey",
-        marginTop: "5rem"
-      }}>Redirecting in {countdown}...</p>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "white",
+        textAlign: "center",
+      }}
+    >
+      <h1
+        style={{
+          color: "black",
+          fontSize: "2.5rem",
+        }}
+      >
+        yAcademy
+      </h1>
+      <p style={{ fontSize: "1.5rem", margin: "10px 0", color: "grey" }}>
+        is now
+      </p>
+      <img
+        src="/electisec-logo.png"
+        alt="Electisec Logo"
+        width={250}
+        height={250}
+      />
+      <p
+        style={{
+          color: "grey",
+          marginTop: "5rem",
+        }}
+      >
+        Redirecting in {countdown}...
+      </p>
     </div>
   );
 }
-
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Home() {
   const pathname = usePathname();
   const [countdown, setCountdown] = useState(3);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       const interval = setInterval(() => {
         setCountdown((prev) => {
           if (prev === 1) {
             clearInterval(interval);
-            const newUrl = `https://reports.electisec.tech${pathname}`;
+            const newUrl = `https://reports.electisec.com${pathname}`;
             window.location.href = newUrl;
           }
           return prev - 1;
@@ -24,26 +24,42 @@ export default function Home() {
   }, [pathname]);
 
   return (
-    <div style={{ 
-      display: 'flex', 
-      flexDirection: 'column', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      height: '100vh', 
-      backgroundColor: 'white', 
-      textAlign: 'center' 
-    }}>
-      <h1 style={{
-        color: "black",
-        fontSize: '2.5rem'
-      }}>yAcademy</h1>
-      <p style={{ fontSize: '1.5rem', margin: '10px 0', color: "grey" }}>is now</p>
-      <img src="/electisec-logo.png" alt="Electisec Logo" width={250} height={250} />
-      <p  style={{
-        color: "grey",
-        marginTop: "5rem"
-      }}>Redirecting in {countdown}...</p>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "white",
+        textAlign: "center",
+      }}
+    >
+      <h1
+        style={{
+          color: "black",
+          fontSize: "2.5rem",
+        }}
+      >
+        yAcademy
+      </h1>
+      <p style={{ fontSize: "1.5rem", margin: "10px 0", color: "grey" }}>
+        is now
+      </p>
+      <img
+        src="/electisec-logo.png"
+        alt="Electisec Logo"
+        width={250}
+        height={250}
+      />
+      <p
+        style={{
+          color: "grey",
+          marginTop: "5rem",
+        }}
+      >
+        Redirecting in {countdown}...
+      </p>
     </div>
   );
 }
-
