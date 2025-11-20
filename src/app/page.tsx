e6f7d64bfb167651393774afa94fe78e617616bb
nullity <70228821+nullity00@users.noreply.github.com>
2025-11-20 15:46:21 +0530

    remove countdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,61 +5,14 @@ import { usePathname } from "next/navigation";
 
 export default function Home() {
   const pathname = usePathname();
-  const [countdown, setCountdown] = useState(3);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      const interval = setInterval(() => {
-        setCountdown((prev) => {
-          if (prev === 1) {
-            clearInterval(interval);
-            const newUrl = `https://reports.yaudit.dev${pathname}`;
-            window.location.href = newUrl;
-          }
-          return prev - 1;
-        });
-      }, 1000);
-      return () => clearInterval(interval);
+      const slug = pathname;
+      if (!slug) return;
+      window.location.href = `https://yaudit.dev${slug}`;
     }
   }, [pathname]);
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        backgroundColor: "white",
-        textAlign: "center",
-      }}
-    >
-      <h1
-        style={{
-          color: "black",
-          fontSize: "2.5rem",
-        }}
-      >
-        yAcademy
-      </h1>
-      <p style={{ fontSize: "1.5rem", margin: "10px 0", color: "grey" }}>
-        is now
-      </p>
-      <img
-        src="/electisec-logo.png"
-        alt="Electisec Logo"
-        width={250}
-        height={250}
-      />
-      <p
-        style={{
-          color: "grey",
-          marginTop: "5rem",
-        }}
-      >
-        Redirecting in {countdown}...
-      </p>
-    </div>
-  );
+  return <></>;
 }
